69d1f8205691f7b4f91675132c914942ebaec2fe
juryt <utroshkin@mail.ru>
2026-01-12 21:32:14 +0300

    Start project

--- src/com/javarush/task/pro/task05/task0510/Solution.java
+++ src/com/javarush/task/pro/task05/task0510/Solution.java
@@ -10,7 +10,7 @@ public class Solution {
 
     public static void main(String[] args) {
         //напишите тут ваш код
-        int[][] array = new int[10][];
+        int[][] array = new int[10][]; //  создали массив
         array[0] = new int[1];
         array[1] = new int[2];
         array[2] = new int[3];
@@ -19,16 +19,16 @@ public class Solution {
         array[5] = new int[6];
         array[6] = new int[7];
         array[7] = new int[8];
-        array[8] = new int[9];
+        array[8] = new int[9];   //  задали размер
         array[9] = new int[10];
 
-        for (int i = 0; i < array.length; i++) {
-            result[i] = new int[i + 1];
+        for (int i = 0; i < array.length; i++) {   // прошлись по длинам строк
+            result[i] = new int[i + 1]; // каждая строка увеличивается на 1
         }
 
-        for (int i = 0; i < result.length; i++) {
+        for (int i = 0; i < result.length; i++) {     // заполнили цифрами строки
             for (int j = 0; j < result[i].length; j++) {
-                result[i][j] = i + j;
+                result[i][j] = i + j; //  присвоили значение ячейкам
                 System.out.print(result[i][j] + " ");
             }
             System.out.println();

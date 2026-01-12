b5dcd2ee54d2e15afc1034a7809036a2a08d471b
juryt <utroshkin@mail.ru>
2026-01-12 20:31:56 +0300

    Start project

---
+++ src/com/javarush/task/pro/task05/task0510/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.pro.task05.task0510;
+
+/* 
+Треугольный массив
+*/
+
+public class Solution {
+
+    public static int[][] result = new int[10][];
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        int[][] array = new int[10][];
+        array[0] = new int[1];
+        array[1] = new int[2];
+        array[2] = new int[3];
+        array[3] = new int[4];
+        array[4] = new int[5];
+        array[5] = new int[6];
+        array[6] = new int[7];
+        array[7] = new int[8];
+        array[8] = new int[9];
+        array[9] = new int[10];
+
+        for (int i = 0; i < array.length; i++) {
+            result[i] = new int[i + 1];
+        }
+
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[i].length; j++) {
+                result[i][j] = i + j;
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
+

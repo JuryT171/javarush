8811d9df0d0670b3c86dd5d2ded41c08a391a998
juryt <utroshkin@mail.ru>
2026-01-12 20:29:16 +0300

    Start project

--- src/com/javarush/task/pro/task05/task0509/Solution.java
+++ src/com/javarush/task/pro/task05/task0509/Solution.java
@@ -7,18 +7,16 @@ package com.javarush.task.pro.task05.task0509;
 public class Solution {
 
     public static int[][] MULTIPLICATION_TABLE;
-    static int x = 13;
-    static int y = 21;
 
     public static void main(String[] args) {
         //напишите тут ваш код
-        MULTIPLICATION_TABLE = new int[10][10];
-        for (int i = 0; i < MULTIPLICATION_TABLE.length; i++) {
-            for (int j = 0; j < MULTIPLICATION_TABLE.length; j++) {
-                MULTIPLICATION_TABLE[i][j] = ((i + 1) * (j + 1));
-                System.out.print(MULTIPLICATION_TABLE[i][j] + " ");
+        MULTIPLICATION_TABLE = new int[10][10];  // присваиваем массиву размер
+        for (int i = 0; i < MULTIPLICATION_TABLE.length; i++) {  // проходимся по строкам
+            for (int j = 0; j < MULTIPLICATION_TABLE.length; j++) {  // проходимся по стоблцам
+                MULTIPLICATION_TABLE[i][j] = ((i + 1) * (j + 1));   // сохраняем результат умножения
+                System.out.print(MULTIPLICATION_TABLE[i][j] + " ");  // выводим на экран
             }
-            System.out.println();
+            System.out.println();  // перенос строки
 
         }
 

2aa0c7dbd50118de524845adc358f9108552401f
juryt <utroshkin@mail.ru>
2026-01-17 18:04:08 +0300

    Start project

---
+++ src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,45 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(){
+
+    }
+
+    //напишите тут ваш код
+    public static void universalMethod(int a){
+
+    }
+    public static void universalMethod(int [] array){
+
+    }
+    public static void universalMethod(int [][] array){
+
+    }
+    public static void universalMethod(String names){
+
+    }
+    public static void universalMethod(String surnames, String ages){
+
+    }
+    public static void universalMethod(int[][][]array){
+
+    }
+    public static void universalMethod(double count){
+
+    }
+    public static void universalMethod(char letters){
+
+    }
+    public static void universalMethod(String cars, String models, String price){
+
+    }
+}

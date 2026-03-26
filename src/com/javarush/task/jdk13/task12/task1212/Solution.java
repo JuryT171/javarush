7aeb5f6821291f13657c81b350a6af058f6bdcaa
juryt <utroshkin@mail.ru>
2026-03-26 18:10:26 +0300

    Start project

---
+++ src/com/javarush/task/jdk13/task12/task1212/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.jdk13.task12.task1212;
+
+/* 
+Пять методов print с разными параметрами
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+    public static void print(char c) {
+    }
+    public static void print(int x, String s) {
+    }
+    public static void print(String d) {
+    }
+    public static void print(Integer o) {
+    }
+    public static void print(Long l) {
+    }
+
+}

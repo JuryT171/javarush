c4961137a30e0e149c7fac7539d5e999171b4a6f
juryt <utroshkin@mail.ru>
2026-03-26 18:07:08 +0300

    Start project

---
+++ src/com/javarush/task/jdk13/task12/task1210/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.jdk13.task12.task1210;
+
+/* 
+print(int) и print(String)
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+    public static void print(int x){
+
+    }
+    public static void print(String s){
+
+    }
+}
---
+++ src/com/javarush/task/jdk13/task12/task1211/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.jdk13.task12.task1211;
+
+/* 
+print(int) и print(Integer)
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    print(42);
+    Integer o = 100;
+    print(o);
+    }
+
+    //Напишите тут ваши методы
+    public static void print(int x){
+    }
+    public static void print(Integer o){
+
+    }
+}

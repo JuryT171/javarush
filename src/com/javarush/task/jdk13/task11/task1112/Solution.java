d70f0dca2f77915b62ea5a8ab5d12c31e8802aca
juryt <utroshkin@mail.ru>
2026-03-29 11:05:32 +0300

    Start project

---
+++ src/com/javarush/task/jdk13/task11/task1112/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.jdk13.task11.task1112;
+
+/* 
+Конструируем дырчик
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Car car = new Car(); //  создаем авто
+        car.start();
+        car.turnOff();
+    }
+}

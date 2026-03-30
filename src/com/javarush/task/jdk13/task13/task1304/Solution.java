8dabca6a287463bfaa40f485a1a49a6d1522576d
juryt <utroshkin@mail.ru>
2026-03-30 17:45:30 +0300

    Start project

---
+++ src/com/javarush/task/jdk13/task13/task1304/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.jdk13.task13.task1304;
+
+/* 
+Жив или нет
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш ко
+    }
+    public interface Person{
+        boolean isAlive();
+
+    }
+    interface Presentable extends Person {
+
+    }
+}

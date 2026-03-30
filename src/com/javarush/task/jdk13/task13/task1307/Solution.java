0885970dd1f07681ad8d5c83c88bc05f570f1550
juryt <utroshkin@mail.ru>
2026-03-30 19:37:47 +0300

    Start project

---
+++ src/com/javarush/task/jdk13/task13/task1307/Solution.java
@@ -0,0 +1,69 @@
+package com.javarush.task.jdk13.task13.task1307;
+
+/* 
+Dog, Cat и Mouse
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //может двигаться
+    public interface Movable {
+        void move();
+    }
+
+    //может быть съеден
+    public interface Edible {
+        void beEaten();
+    }
+
+    //может кого-нибудь съесть
+    public interface Eat {
+        void eat();
+    }
+    public class Cat implements Movable,Eat,Edible{
+
+        @Override
+        public void eat() {
+
+        }
+
+        @Override
+        public void beEaten() {
+
+        }
+
+        @Override
+        public void move() {
+
+        }
+    }
+    public class Mouse implements Movable,Edible{
+
+        @Override
+        public void beEaten() {
+
+        }
+
+        @Override
+        public void move() {
+
+        }
+    }
+    public class Dog implements Movable,Eat {
+
+
+
+        @Override
+        public void move() {
+
+        }
+
+        @Override
+        public void eat() {
+
+        }
+    }
+}

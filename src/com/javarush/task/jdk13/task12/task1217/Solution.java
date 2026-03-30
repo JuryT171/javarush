0c1f67ce95e9ffbc90eaa3bb1ebb80906bdee63d
juryt <utroshkin@mail.ru>
2026-03-30 17:10:28 +0300

    Start project

---
+++ src/com/javarush/task/jdk13/task12/task1217/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.jdk13.task12.task1217;
+
+/* 
+I believe I can fly
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //add an interface here - добавь интерфейс тут
+    public interface CanFly{
+        void move(String address);
+        void speed(int speed);
+    }
+
+}

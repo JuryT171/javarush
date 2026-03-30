fbcdb601b571bb817b0b2e3ffbaeb56526bc015f
juryt <utroshkin@mail.ru>
2026-03-30 17:25:30 +0300

    Start project

---
+++ src/com/javarush/task/jdk13/task12/task1220/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.jdk13.task12.task1220;
+
+/* 
+Почти что Iron Man
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //add public interfaces and a public class here - добавь public интерфейсы и public класс тут
+    public abstract class Human implements CanRun,CanSwim{
+
+    }
+    public interface CanRun{
+        void run();
+    }
+    public interface CanSwim{
+        void swim();
+    }
+}

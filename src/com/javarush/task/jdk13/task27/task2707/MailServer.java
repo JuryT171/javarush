f56163397f212db3b063466adf77ff127f2e00db
juryt <utroshkin@mail.ru>
2026-04-27 20:47:43 +0300

    Start project

---
+++ src/com/javarush/task/jdk13/task27/task2707/MailServer.java
@@ -0,0 +1,25 @@
+package com.javarush.task.jdk13.task27.task2707;
+
+public class MailServer implements Runnable {
+    private Mail mail;
+
+    public MailServer(Mail mail) {
+        this.mail = mail;
+    }
+
+    @Override
+    public void run() {
+        long startTime = System.currentTimeMillis();
+        //сделайте что-то тут - do something here
+        try{
+        synchronized (mail){
+           mail.wait();  // освобождаем майл для другого потока
+           }
+            String name = Thread.currentThread().getName();
+            long endTime = System.currentTimeMillis();
+            System.out.format("%s MailServer received: [%s] in %d ms after start", name, mail.getText(), (endTime - startTime));
+        } catch (InterruptedException ignored){
+
+        }
+    }
+}

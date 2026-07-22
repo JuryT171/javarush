e55fadeaab9c6c1674a299f03132e3a1f0846669
juryt <utroshkin@mail.ru>
2026-07-22 22:20:09 +0300

    Start project

---
+++ src/com/javarush/task/jdk13/task41/task4117/Chat.java
@@ -0,0 +1,20 @@
+package com.javarush.task.jdk13.task41.task4117;
+
+public class Chat implements Mediator{
+    private UserDB db;
+
+    public Chat(UserDB db) {
+        this.db = db;
+    }
+
+    @Override
+    public void sendMessage(String msg, int userId) {
+        User u = db.getById(userId);
+        u.receive(msg);
+    }
+
+    @Override
+    public void register(User user) {
+        db.add(user);
+    }
+}

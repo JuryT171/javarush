0303f117cc083b6b5666caa7da4fac2a8fe18cb9
juryt <utroshkin@mail.ru>
2026-07-23 09:42:58 +0300

    player

---
+++ src/com/javarush/task/jdk13/task41/task4118/states/State.java
@@ -0,0 +1,15 @@
+package com.javarush.task.jdk13.task41.task4118.states;
+
+import com.javarush.task.jdk13.task41.task4118.ui.Player;
+
+public abstract class State {
+    protected Player player;
+
+    public State(Player player) {
+        this.player = player;
+    }
+    public abstract String onLock();
+    public abstract String onPlay();
+    public abstract String onNext();
+    public abstract String onPrevious();
+}

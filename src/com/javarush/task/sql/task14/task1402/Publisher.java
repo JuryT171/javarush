96bfcc05fb7a0a504df1336fc620a7a02cf91af2
juryt <utroshkin@mail.ru>
2026-09-13 19:26:37 +0300

    @LazyCollection

---
+++ src/com/javarush/task/sql/task14/task1402/Publisher.java
@@ -0,0 +1,50 @@
+package com.javarush.task.sql.task14.task1402;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+
+import java.util.List;
+
+@Entity
+@Table(name = "publisher")
+public class Publisher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    // Если параметр установлен в LazyCollectionOption.FALSE,
+    // то это значит, что значения поля comments будут загружены из базы
+    // в момент загрузки родительского объекта
+    @LazyCollection(LazyCollectionOption.FALSE)
+    @JoinColumn(name = "publisher_id")
+    private List<Book> books;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+}
